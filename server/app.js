39c67fd4dcf058cfdf408465f67babcd760752c5
Lucas <lucasalonso11800@gmail.com>
2021-07-20 10:46:05 -0300

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const { graphqlHTTP } = require('express-graphql');
+
+const app = express();
+app.use(cors());
+
+// app.use('/graphql', graphqlHTTP({
+//     schema,
+//     graphiql: true
+// }))
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
